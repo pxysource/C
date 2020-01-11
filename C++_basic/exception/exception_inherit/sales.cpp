5d2278e09a559758e1e28745f3b045b60892f802
pxysource <panxingyuan1@163.com>
2020-01-11 21:26:54 +0800

    delete C++_base

--- C++_basic/exception/exception_inherit/sales.cpp
+++
@@ -1,6 +0,0 @@
-#include "sales.h"
-
-Sales::Sales()
-{
-
-}
--- C++_basic/function/inline_func/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "max.h"
-
-void print(int a, int b)
-{
-    using std::cout;
-    using std::endl;
-    cout << max(a, b) << endl;
-}
